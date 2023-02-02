c1a5ee657e279a6d7c3922d75b97c7cda3259637
Egor51 <kubrakov_egor87@mail.ru>
2023-02-02 20:03:38 +0300

    create method hello

--- src/main/java/com/example/orm/controller/PersonController.java
+++ src/main/java/com/example/orm/controller/PersonController.java
@@ -4,8 +4,7 @@ import com.example.orm.model.Person;
 import com.example.orm.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +36,10 @@ public class PersonController {
     public Optional<Person> findPersonByAge(@RequestParam String name,
                                             @RequestParam String surname){
         return personService.getPersonByNameAndSurname(name,surname);
+    }
+    @GetMapping("/hello/")
+    public String getHello(){
+        return "Hello";
     }
 }
 
