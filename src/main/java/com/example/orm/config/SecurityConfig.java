3149e18bbd3ec02f8542e8ca9946c95e2986d11a
Egor51 <kubrakov_egor87@mail.ru>
2023-02-02 20:03:56 +0300

    create SecurityConfig

---
+++ src/main/java/com/example/orm/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.orm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/persons/by-city/").hasAuthority("ROLE_USER")
+                        .requestMatchers("/persons/by-age/").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/hello/").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .formLogin();
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}password")
+                .authorities("ROLE_USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}password")
+                .authorities("ROLE_ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
+
+
