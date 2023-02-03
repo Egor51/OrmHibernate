c369f76b7f4aece0dfe96bd3f2dea4b060ff64d5
Egor51 <kubrakov_egor87@mail.ru>
2023-02-03 15:46:16 +0300

    create annotation spring-secure

--- src/main/java/com/example/orm/controller/PersonController.java
+++ src/main/java/com/example/orm/controller/PersonController.java
@@ -2,9 +2,13 @@ package com.example.orm.controller;
 
 import com.example.orm.model.Person;
 import com.example.orm.service.PersonService;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,23 +26,27 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @RolesAllowed({"ROLE_USER"})
     @GetMapping("/persons/by-city/")
     public Optional<List<Person>> getPerson(@RequestParam String city) {
         return personService.getPersonByCity(city);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @GetMapping("/persons/by-age/")
-    public Optional<List<Person>> findPersonByAge(@RequestParam int age){
+    public Optional<List<Person>> findPersonByAge(@RequestParam int age) {
         return personService.getPersonByAge(age);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')||hasRole('ROLE_USER')")
     @GetMapping("/persons/by-name/")
     public Optional<Person> findPersonByAge(@RequestParam String name,
-                                            @RequestParam String surname){
-        return personService.getPersonByNameAndSurname(name,surname);
+                                            @RequestParam String surname) {
+        return personService.getPersonByNameAndSurname(name, surname);
     }
+    @PostAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/hello/")
-    public String getHello(){
+    public String getHello() {
         return "Hello";
     }
 }
